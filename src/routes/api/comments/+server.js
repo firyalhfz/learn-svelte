6a3c9bec7e751bced4644c85292bd32a420c2575
firyalhfz <iyaalfiryal@gmail.com>
2024-07-22 23:07:14 +0700

    feat: api handle post request

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -9,3 +9,12 @@ export function GET() {
   //its same code like above
   return json(comments);
 }
+
+export async function POST(requestEvent) {
+  const { request } = requestEvent;
+  const { text } = await request.json();
+  const newComment = { id: comments.length + 1, text };
+  comments.push(newComment);
+  //   return new Response(JSON.stringify(newComment), { status: 200 });
+  return json(newComment, { status: 200 });
+}
