9123bb92a5cfd75f179254d27b4d5ae54f31ad40
firyalhfz <iyaalfiryal@gmail.com>
2024-07-22 23:19:09 +0700

    feat: api handle get detail comment request

---
+++ src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,9 @@
+import { comments } from '$lib/comments';
+import { json } from '@sveltejs/kit';
+
+export function GET(requestEvent) {
+  const { params } = requestEvent;
+  const { commentId } = params;
+  const comment = comments.find((comment) => comment.id === parseInt(commentId));
+  return json(comment);
+}
